3d14fe579da282020fd0acd6d657222bf96cb00d
zahmoulovski <medyassine.zahmoul@gmail.com>
2022-09-26 02:18:25 +0100

    adding functions to the city buttons

---
+++ js/script.js
@@ -0,0 +1,79 @@
+function warsaw() {
+  let text;
+  if (confirm("Press a button!") == true) {
+    text = "Poland!";
+  } else {
+    text = "You canceled!";
+  }
+  document.getElementById("warsaw").innerHTML = text;
+}
+
+function barca() {
+  let text;
+  if (confirm("Press a button!") == true) {
+    text = "Spain!";
+  } else {
+    text = "You canceled!";
+  }
+  document.getElementById("barca").innerHTML = text;
+}
+
+function munich() {
+  let text;
+  if (confirm("Press a button!") == true) {
+    text = "Germany!";
+  } else {
+    text = "You canceled!";
+  }
+  document.getElementById("munich").innerHTML = text;
+}
+
+function istanbul() {
+  let text;
+  if (confirm("Press a button!") == true) {
+    text = "Turkey!";
+  } else {
+    text = "You canceled!";
+  }
+  document.getElementById("istanbul").innerHTML = text;
+}
+
+function rome() {
+  let text;
+  if (confirm("Press a button!") == true) {
+    text = "Ilaty!";
+  } else {
+    text = "You canceled!";
+  }
+  document.getElementById("rome").innerHTML = text;
+}
+
+function bologna() {
+  let text;
+  if (confirm("Press a button!") == true) {
+    text = "Italy!";
+  } else {
+    text = "You canceled!";
+  }
+  document.getElementById("bologna").innerHTML = text;
+}
+
+function curitiba() {
+  let text;
+  if (confirm("Press a button!") == true) {
+    text = "Brazil!";
+  } else {
+    text = "You canceled!";
+  }
+  document.getElementById("curitiba").innerHTML = text;
+}
+
+function mexicoCity() {
+  let text;
+  if (confirm("Press a button!") == true) {
+    text = "Mexico!";
+  } else {
+    text = "You canceled!";
+  }
+  document.getElementById("mexicoCity").innerHTML = text;
+}
